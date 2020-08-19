f32f4b6ab7138a86ad58057db0e17c5db7c97701
Rohil Tuli <rohil.tuli@gmail.com>
2020-08-18 23:28:14 -0400

    Initial plan for website

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+        'display': ['Abril Fatface'],
+        'body': ['Open Sans'],
+    }
+  },
+  variants: {},
+  plugins: [],
+}
